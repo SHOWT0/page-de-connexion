d0996d6524dff74ab7894f943eca5f8d553ef28d
Showto <92624737+SHOWT0@users.noreply.github.com>
2023-10-07 16:28:09 +0200

    first commit

---
+++ register.js
@@ -0,0 +1,7 @@
+const background = document.querySelector('.background');
+
+document.addEventListener('mousemove', (e) => {
+    const mouseX = e.clientX / window.innerWidth;
+    const mouseY = e.clientY / window.innerHeight;
+    background.style.transform = `translate(-${mouseX * 50}px, -${mouseY * 50}px)`;
+});
